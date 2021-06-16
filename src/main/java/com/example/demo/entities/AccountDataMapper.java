d708080b558f83d0b400357031609a04f9306691
Gökalp Rençber <grencber@gmail.com>
2021-06-16 22:10:01 +0300

    Entity classes for Account, Customer and Transaction are added.

---
+++ src/main/java/com/example/demo/entities/AccountDataMapper.java
@@ -0,0 +1,58 @@
+package com.example.demo.entities;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
+public class AccountDataMapper {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    BigDecimal balance;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    CustomerDataMapper customer;
+
+    public AccountDataMapper() {
+    }
+
+    public AccountDataMapper(BigDecimal balance, CustomerDataMapper customerDataMapper) {
+        this.balance = balance;
+        this.customer = customerDataMapper;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public CustomerDataMapper getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(CustomerDataMapper customer) {
+		this.customer = customer;
+	}
+    
+    
+}
+
---
+++ src/main/java/com/example/demo/entities/TransactionDataMapper.java
@@ -0,0 +1,84 @@
+package com.example.demo.entities;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.Nullable;
+
+@Entity
+@Table(name = "transaction")
+public class TransactionDataMapper {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Nullable
+	private Integer accountFrom;
+	
+	private Integer accountTo;
+	
+	private BigDecimal amount;
+	
+	private Date date;
+	
+	public TransactionDataMapper() {
+
+	}
+
+	public TransactionDataMapper(Integer accountFrom, Integer accountTo, BigDecimal amount, Date date) {
+		this.accountFrom = accountFrom;
+		this.accountTo = accountTo;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getAccountFrom() {
+		return accountFrom;
+	}
+
+	public void setAccountFrom(Integer accountFrom) {
+		this.accountFrom = accountFrom;
+	}
+
+	public Integer getAccountTo() {
+		return accountTo;
+	}
+
+	public void setAccountTo(Integer accountTo) {
+		this.accountTo = accountTo;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
+	
+	
